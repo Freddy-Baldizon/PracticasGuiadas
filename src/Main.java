d60e8d9f8bf7af89adf17d934bbfd1b69edd2344
Freddy-Baldizon <freddybladizon@gmail.com>
2026-01-12 14:45:46 -0600

    Ejercicios de arrays

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int[] numbers = {10,20,30,40,50};
+        String[] fruits = new String[3];
+
+        System.out.println(numbers[2] + " - " + fruits[2]);
+    }
+
+   
+}
