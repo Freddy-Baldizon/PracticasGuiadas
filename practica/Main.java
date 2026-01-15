5f07cc39a27ab1a46d009b25de1a728dc7844982
Freddy-Baldizon <freddybladizon@gmail.com>
2026-01-14 20:57:31 -0600

    Practica de arreglos

---
+++ practica/Main.java
@@ -0,0 +1,77 @@
+package practica;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        valorAlto();
+        valorBajo();
+        segundoMayor();
+        suma();
+        cuadrado();
+    }
+
+    public static void valorAlto() {
+        int[] numbers = { 1, 20, 3, 4, 5 };
+        int mayor = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > mayor) {
+                mayor = numbers[i];
+            }
+        }
+        System.out.println("El numero mayor es: " + mayor);
+
+    }
+
+    public static void valorBajo() {
+        int[] numbers = { 10, 20, 3, 40, 50 };
+        int bajo = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < bajo) {
+                bajo = numbers[i];
+            }
+        }
+        System.out.println("El numero menor es: " + bajo);
+
+    }
+
+    public static void cuadrado() {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int[] cuadrado = Arrays.copyOf(numbers, numbers.length);
+
+        for (int i = 0; i < numbers.length; i++) {
+            cuadrado[i] *= numbers[i]; 
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("EL arreglo original es: " + numbers[i]);
+        }
+
+        for (int i = 0; i < cuadrado.length; i++) {
+            System.out.println("EL arreglo al cuadrado es: " + cuadrado[i]);
+        }
+    }
+
+    public static void suma() {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int suma = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            suma += numbers[i];
+        }
+        System.out.println("La suma total es: " + suma);
+    }
+    // falta saber como sacar el segundo mayor de los arrays
+    public static void segundoMayor(){
+        int[] numbers = { 1, 2, 3, 40, 5 };
+        int mayor = numbers[0];
+        int segundoMayor = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > mayor) {
+                mayor = numbers[i];
+                segundoMayor = mayor;
+            }
+        }
+        System.out.println("El segundo numero mayor es: " + mayor);
+    }
+
+}
